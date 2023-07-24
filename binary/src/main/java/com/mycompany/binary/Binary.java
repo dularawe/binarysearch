26139d4c4ea92834a864c371e783727815fb0e2f
Dulara Pramod <95749652+dularawe@users.noreply.github.com>
2023-07-25 00:03:23 +0530

    Add files via upload

---
+++ binary/src/main/java/com/mycompany/binary/Binary.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.binary;
+
+/**
+ *
+ * @author ACER
+ */
+public class Binary {
+
+    public static void main(String[] args) {
+      tool obj =new tool();
+      obj.show();
+    }
+          
+         
+          
+    }
+
